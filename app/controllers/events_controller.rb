394963b647f606297a6288de085d0d5f89921654
veritywong <veritywong90@gmail.com>
2024-12-15 22:15:16 +0000

    events form v1

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,12 @@
 class EventsController < ApplicationController
-    before_action :find_event, only: [:show, :edit]
+    before_action :find_event, only: [:show, :edit, :update]
     def index
         @events = Event.all
     end
 
     def show
         @artists = @event.artists
+        @images = @event.images
         @event.event_artists.build.build_artist if @event.event_artists.empty?
     end
 
@@ -19,22 +20,11 @@ class EventsController < ApplicationController
     end
 
     def create
-        puts "0 -- #{event_params}"
-        puts "1 -- #{event_params.dig("event_artists_attributes", "0", "artist", "name")}"
-
-        artist_names = event_params.dig("event_artists_attributes")&.values&.map do |event_artist|
-            event_artist.dig("artist", "name")
+        artist_names = event_params[:event_artists_attributes]&.values&.map do |event_artist|
+            event_artist[:artist]
         end.reject(&:blank?)
-          
-        puts "2 -- #{artist_names.inspect}"
-        puts "3 -- #{event_params[:event_artists_attributes]}"
-        puts "4 -- #{event_params.dig("event_artists_attributes")}"
-        puts "5 -- #{artist_names.empty?}"
-        if artist_names.empty?
-            @event = Event.new(event_params) 
-        else 
-            @event = Event.new(event_params.except(:event_artists_attributes))
-        end
+ 
+        @event = Event.new(event_params.except(:event_artists_attributes))       
 
         if @event.save
             if artist_names.present?
@@ -43,7 +33,7 @@ class EventsController < ApplicationController
                   EventArtist.create(event: @event, artist: artist)  
                 end
             end
-            attach_saved_images
+            # attach_saved_images
             redirect_to events_path, notice: 'Event was successfully created.'
         else
             render :new, status: :unprocessable_entity
@@ -51,6 +41,11 @@ class EventsController < ApplicationController
     end
 
     def update
+        if @event.update(event_params.except(:event_artists_attributes))
+            redirect_to event_path(@event), notice: 'Event was successfully update.'
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     def attach_saved_images
@@ -70,8 +65,7 @@ class EventsController < ApplicationController
         params.require(:event).permit(
             :title, :url, :gallery_id, :visited_at, :start_date, :end_date, images: [],
             event_artists_attributes: [
-                :id, :_destroy, 
-                artist: [:id, :name, :_destroy]
+                :id, :artist, :_destroy
             ]
             )
     end
